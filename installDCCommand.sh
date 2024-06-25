42f635a0d5b5f9d6cb9998090f179be13cff9fa5
Michael Redig <mredig@gmail.com>
2024-06-25 18:20:29 -0500

    (feat) added installDCCommand

---
+++ installDCCommand.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+echo "alias dc='podman-compose'
+alias ga='git add'
+alias gc='git commit -m'
+alias gp='git push'
+alias gs='git status'
+" >> ~/.profile
+
+source ~/.profile
+
