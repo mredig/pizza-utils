23d1f3895e3b9f47f35a0a72320ceec74dabd757
Michael Redig <mredig@gmail.com>
2024-07-15 00:57:12 -0500

    (refactor) remote install can install git if needed

--- remote-install.sh
+++ remote-install.sh
@@ -4,6 +4,34 @@ set -x
 
 cd /usr/local/src || exit 1
 
+if ! which git; then
+	#install git
+
+	. /etc/os-release
+
+	case "$ID" in
+		ubuntu|debian)
+			apt-get update
+			apt-get install -y git
+		;;
+		fedora|centos)
+			if command -v dnf > /dev/null 2>&1; then
+				dnf install -y git
+			else
+				yum install -y git
+			fi
+		;;
+		alpine)
+			apk update
+			apk add git
+		;;
+		*)
+			echo "Unsupported distro"
+			exit 1
+		;;
+	esac
+fi
+
 if [ ! -d pizza-utils ]; then
 	sudo git clone https://gitlab.com/mredig/pizza-utils
 fi
