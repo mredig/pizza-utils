2ab30dc37e2605ad234d25ae447a15463bed220d
Michael Redig <mredig@gmail.com>
2024-09-27 18:45:59 -0500

    (feat) secret setup compatible with podman and other upgrades

--- secret-setup.py
+++ secret-setup.py
@@ -11,28 +11,31 @@ import subprocess
 # then run `secret-setup.py -s` to initially set up your secrets. If you need to 
 # change any, run `secret-setup.py -u [secretName]`
 
-def checkIfSecretExists(secretName):
-    command = f"docker secret inspect '{secretName}'"
-    result = subprocess.run(command, shell = True, executable="/bin/sh")
+def checkIfSecretExists(secretName, toolName):
+    # command = f"docker secret inspect '{secretName}'"
+    command = [toolName, "secret", "inspect", secretName]
+    result = __runCommand(command)
     return result.returncode == 0
 
-def deleteSecret(secretName):
-    command = f"docker secret rm '{secretName}'"
-    result = subprocess.run(command, shell = True, executable="/bin/sh")
+def deleteSecret(secretName, toolName):
+    # command = f"docker secret rm '{secretName}'"
+    command = [toolName, "secret", "rm", secretName]
+    result = __runCommand(command)
     return result.returncode == 0
 
-def saveSecret(name, secret):
+def saveSecret(name, secret, toolName):
     with open('/tmp/pw.tmp', 'w') as f:
         f.write(secret)
 
-    command = f"docker secret create {name} /tmp/pw.tmp"
-    result = subprocess.run(command, shell = True, executable="/bin/sh")
+    # command = f"docker secret create {name} /tmp/pw.tmp"\
+    command = [toolName, "secret", "create", name, "/tmp/pw.tmp"]
+    result = __runCommand(command)
     os.remove("/tmp/pw.tmp")
     if result.returncode != 0:
         print(f"Error creating secret {name}")
         exit(1)
 
-def retrieveSecret(secretName, verify=True):
+def retrieveSecret(secretName, toolName, verify=True):
     secret = getpass.getpass(f"Enter secret for {secretName}: ")
 
     if verify:
@@ -40,48 +43,91 @@ def retrieveSecret(secretName, verify=True):
 
     return secret
 
-def setupSecrets(secretlist):
+def setupSecrets(secretlist, toolName):
     for secretname in secretlist:
-        secret = retrieveSecret(secretname)
+        secret = retrieveSecret(secretname, toolName)
 
-        if checkIfSecretExists(secretname) == False:
-            saveSecret(secretname, secret)
+        if checkIfSecretExists(secretname, toolName) == False:
+            saveSecret(secretname, secret, toolName)
         else:
             print(f"Secret {secretname} already exists. Skipping.")
 
-def updateSecret(secretName):
-    updatedValue = retrieveSecret(secretName)
-    if checkIfSecretExists(secretName):
-        if deleteSecret(secretName):
+def updateSecret(secretName, toolName):
+    updatedValue = retrieveSecret(secretName, toolName)
+    if checkIfSecretExists(secretName, toolName):
+        if deleteSecret(secretName, toolName):
             print(f"deleted previous {secretName}")
         else:
             print(f"Error removing existing {secretName}")
             exit(1)
-    saveSecret(secretName, updatedValue)
+    saveSecret(secretName, updatedValue, toolName)
 
-parser = argparse.ArgumentParser(description="Setup secrets")
+def isToolInstalled(toolName):
+    try:
+        command = ['command', '-v', toolName]
+        result = __runCommand(command, False)
+        return result.returncode == 0
+    except Exception as e:
+        return False
 
-parser.add_argument('--setup', "-s", action="store_true", help="Setup secrets initially")
-parser.add_argument('--update', "-u", help="Update provided secret")
+def getTool(toolName):
+    if isToolInstalled(toolName):
+        return toolName
+    
+    known = set()
+    known.add("docker")
+    known.add("podman")
+    known.remove(toolName)
 
-validSecrets = []
-try:
-    with open('secretlist') as f:
-        for line in f.readlines():
-            validSecrets.append(line.strip())
-except FileNotFoundError as error:
-    print("'secretlist' doesn't exist. Please create 'secretlist' with each secret name on a new line.")
+    for tool in known:
+        if isToolInstalled(tool):
+            userinput = input(f"{toolName} isn't installed, but {tool} is. Proceed with {tool}? [Y/n]")
+            if userinput.lower() != "n":
+                return tool
+    print(f"Can't find {toolName}. Please try again")
     exit(1)
 
-args = parser.parse_args()
+def __runCommand(command, printOutput=True):
+    try:
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if printOutput:
+            print(result.stdout)
+        return result
+    except Exception as e:
+        return CompetedProcess()
 
-if (args.setup):
-    setupSecrets(validSecrets)
-    exit(0)
+def main():
+    parser = argparse.ArgumentParser(description="Setup secrets")
 
-if args.update is not None and args.update in validSecrets:
-    updateSecret(args.update)
-    exit(0)
+    parser.add_argument('--setup', "-s", action="store_true", help="Setup secrets initially")
+    parser.add_argument('--update', "-u", help="Update provided secret")
+    parser.add_argument('--tool', "-t", default="docker", help="Use this tool to manage secrets (must be docker compatible)")
 
-print("You must either specify `-s` or `-u [secretname]`.")
-exit(1)
+    validSecrets = []
+    try:
+        with open('secretlist') as f:
+            for line in f.readlines():
+                validSecrets.append(line.strip())
+    except FileNotFoundError as error:
+        print("'secretlist' doesn't exist. Please create 'secretlist' with each secret name on a new line.")
+        exit(1)
+
+    args = parser.parse_args()
+    toolName = args.tool
+
+    toolName = getTool(toolName)
+
+    if (args.setup):
+        setupSecrets(validSecrets, toolName)
+        exit(0)
+
+    if args.update is not None and args.update in validSecrets:
+        updateSecret(args.update, toolName)
+        exit(0)
+
+    print("You must either specify `-s` or `-u [secretname]`.")
+    exit(1)
+
+
+if __name__ == '__main__':
+    main()
